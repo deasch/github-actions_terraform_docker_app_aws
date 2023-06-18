10fda541dfa5a73b2661463ea3b574203965dd0d
Dennis Schmid <124032805+deasch@users.noreply.github.com>
2023-06-18 20:53:57 +0200

    Update main.tf

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,1 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
 
+# Configure the AWS Provider
+provider "aws" {
+  region = "eu-central-1"
+}
+
+# Create a VPC
+resource "aws_vpc" "aws_sandbox_vpc" {
+  cidr_block = "10.0.0.0/16"
+}
