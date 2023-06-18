755b8907fc63c0b2ffc1ae1397aaf6c60b347142
Dennis Schmid <124032805+deasch@users.noreply.github.com>
2023-06-18 21:20:37 +0200

    Update main.tf

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -15,4 +15,10 @@ provider "aws" {
 # Create a VPC
 resource "aws_vpc" "aws_sandbox_vpc" {
   cidr_block = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+  tags = {
+    Name        = "aws_sandbox_vpc"
+    Environment = "aws_sandbox"
+  }
 }
