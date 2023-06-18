c2b37d61619ff1f19ddf1b8a6d1fa1874a298310
Dennis Schmid <124032805+deasch@users.noreply.github.com>
2023-06-18 23:57:32 +0200

    Update main.tf

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,1 +1,23 @@
+variable "repo_version"{
+  default = "v0.0.0.1"
+}
 
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+
+# ========== PROVIDER
+# ===== AWS
+provider "aws" {
+  region = "eu-central-1"
+}
+
+
+# ========== MODULES
