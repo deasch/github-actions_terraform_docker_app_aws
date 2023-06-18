123c10173acef659ebc7ad203bbb6ef8572d7f97
Dennis Schmid <124032805+deasch@users.noreply.github.com>
2023-06-18 21:31:23 +0200

    Update main.tf

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -26,3 +26,36 @@ resource "aws_vpc" "aws_sandbox_vpc" {
     Environment = "aws_sandbox"
   }
 }
+
+
+# ========== NETWORKING - SUBNETS
+# ===== PUBLIC SUBNET
+resource "aws_subnet" "aws_sandbox_public_subnet" {
+  vpc_id                  = aws_vpc.aws_sandbox_vpc.id
+  count                   = "${length(var.public_subnets_cidr)}"
+  cidr_block              = "${element(var.public_subnets_cidr,   count.index)}"
+  availability_zone       = "${element(var.availability_zones,   count.index)}"
+  map_public_ip_on_launch = true
+  tags = {
+    Name        = "aws_sandbox-${element(var.availability_zones, count.index)}-public-subnet"
+    Environment = "aws_sandbox"
+  }
+}
+# ===== PRIVATE SUBNET
+resource "aws_subnet" "aws_sandbox_private_subnet" {
+  vpc_id                  = aws_vpc.aws_sandbox_vpc.id
+  count                   = "${length(var.private_subnets_cidr)}"
+  cidr_block              = "${element(var.private_subnets_cidr, count.index)}"
+  availability_zone       = "${element(var.availability_zones,   count.index)}"
+  map_public_ip_on_launch = false
+  tags = {
+    Name        = "aws_sandbox-${element(var.availability_zones, count.index)}-private-subnet"
+    Environment = "aws_sandbox"
+  }
+}
+
+
+
+
+
+
