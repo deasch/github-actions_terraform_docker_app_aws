ae747d80d9be97a68ada67a26686864b8292583b
Dennis Schmid <124032805+deasch@users.noreply.github.com>
2023-06-18 23:36:40 +0200

    Update main.tf

--- compute/main.tf
+++ compute/main.tf
@@ -20,11 +20,30 @@ provider "aws" {
 }
 
 
-# ========== REPOSITORY
+# ========== COMPUTE - REPOSITORY
 # ===== ECR
 resource "aws_ecr_repository" "aws_sandbox_ecr" {
   name                 = "aws_sandbox_ecr"
   image_tag_mutability = "MUTABLE"
+
+  tags = {
+    Project     = "aws_sandbox"
+    Name        = "aws_sandbox_ecr"
+    Environment = "aws_sandbox"
+  }
+}
+
+
+# ========== COMPUTE - INSTANCES
+# ===== EC2 - APP Server
+resource "aws_instance" "web" {
+  count         = 1 
+  ami           = "ami-0b2ac948e23c57071"
+  instance_type = "t2.micro"
+  
+  key_name      = "devops_sandbox_aws"
+  subnet_id     = aws_subnet.aws_sandbox_public_subnet[0].id
+  vpc_security_group_ids = [aws_security_group.aws_sandbox_default_sg.id]
 
   tags = {
     Project     = "aws_sandbox"
